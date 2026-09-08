252fa411ff3ae23b679566cb54320d59f67b2417
Aggelos Gkiose <it2023009@hua.gr>
2026-09-08 22:58:50 +0300

    Add reservation expiration tests

---
+++ backend/tests/integration/test_expire_reservation_service.py
@@ -0,0 +1,296 @@
+from datetime import datetime, timedelta
+from threading import Thread,Barrier
+
+from zoneinfo import ZoneInfo
+
+import pytest
+from fastapi import HTTPException
+
+from app.database import SessionLocal
+from app.models import ReservationModel, EventSeatModel, EventModel, SeatModel, VenueModel, UserModel
+from app.models.eventseat import EventSeatStatus
+from app.models.reservation import ReservationStatus
+from app.services.reservation_service import expire_reservation_service, cancel_reservation_service
+
+results=[]
+def test_expire_reservation_expires_pending_reservation():
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0,status=EventSeatStatus.HELD)
+        db.add(event_seat1)
+        db.flush()
+        reservation=ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,expires_at = datetime.now(ZoneInfo("Europe/Athens")) - timedelta(minutes=1))
+        db.add(reservation)
+        db.flush()
+        expire_reservation_service(reservation.id,db)
+        assert reservation.status==ReservationStatus.EXPIRED
+        assert event_seat1.status==EventSeatStatus.AVAILABLE
+        assert event_seat1.hold_expires_at  is  None
+    finally:
+        db.rollback()
+        db.close()
+
+def test_expire_reservation_returns_404_when_reservation_not_found():
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0, status=EventSeatStatus.HELD)
+        db.add(event_seat1)
+        db.flush()
+        reservation = ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,
+                                       expires_at=datetime.now(ZoneInfo("Europe/Athens")) - timedelta(minutes=1))
+        db.add(reservation)
+        db.flush()
+        with pytest.raises(HTTPException) as exc:
+            expire_reservation_service(-1, db)
+        assert exc.value.status_code == 404
+    finally:
+        db.rollback()
+        db.close()
+
+def  test_expire_reservation_returns_409_when_reservation_is_not_pending():
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0, status=EventSeatStatus.HELD)
+        db.add(event_seat1)
+        db.flush()
+        reservation = ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,
+                                       expires_at=datetime.now(ZoneInfo("Europe/Athens")) - timedelta(minutes=1),status=ReservationStatus.EXPIRED)
+        db.add(reservation)
+        db.flush()
+        with pytest.raises(HTTPException) as exc:
+            expire_reservation_service(reservation.id, db)
+        assert exc.value.status_code == 409
+    finally:
+        db.rollback()
+        db.close()
+
+def test_expire_reservation_returns_409_when_reservation_is_not_expired_yet():
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0,status=EventSeatStatus.HELD)
+        db.add(event_seat1)
+        db.flush()
+        reservation=ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,expires_at = datetime.now(ZoneInfo("Europe/Athens")) + timedelta(minutes=1))
+        db.add(reservation)
+        db.flush()
+        with pytest.raises(HTTPException) as exc:
+            expire_reservation_service(reservation.id,db)
+        assert exc.value.status_code == 409
+    finally:
+        db.rollback()
+        db.close()
+
+def test_expire_reservation_returns_409_when_event_seat_is_not_held():
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0, status=EventSeatStatus.AVAILABLE)
+        db.add(event_seat1)
+        db.flush()
+        reservation = ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,
+                                       expires_at=datetime.now(ZoneInfo("Europe/Athens")) - timedelta(minutes=1))
+        db.add(reservation)
+        db.flush()
+        with pytest.raises(HTTPException) as exc:
+            expire_reservation_service(reservation.id, db)
+        assert exc.value.status_code == 409
+    finally:
+        db.rollback()
+        db.close()
+
+def test_expire_reservation_rolls_back_on_failure(monkeypatch):
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0, status=EventSeatStatus.HELD,hold_expires_at=datetime.now(ZoneInfo("Europe/Athens"))+timedelta(minutes=10))
+        db.add(event_seat1)
+        db.flush()
+        event_seat1_id=event_seat1.id
+        reservation = ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,
+                                       expires_at=datetime.now(ZoneInfo("Europe/Athens")) - timedelta(minutes=1))
+        db.add(reservation)
+        db.flush()
+        reservation_id=reservation.id
+        db.commit()
+        def fake_commit():
+            raise Exception("Commit failed")
+        monkeypatch.setattr(db, "commit", fake_commit)
+        with pytest.raises(Exception) :
+            expire_reservation_service(reservation.id, db)
+        check_db=SessionLocal()
+        try:
+            reservation = check_db.query(ReservationModel).filter(ReservationModel.id == reservation_id).first()
+            saved_event_seat1 = check_db.query(EventSeatModel).filter(EventSeatModel.id == event_seat1_id).first()
+            assert reservation.status == ReservationStatus.PENDING
+            assert saved_event_seat1.status == EventSeatStatus.HELD
+            assert saved_event_seat1.hold_expires_at is not None
+        finally:
+            check_db.close()
+
+    finally:
+        db.rollback()
+        db.close()
+
+def test_expire_reservation_prevents_concurrent_expiration():
+    results.clear()
+    db = SessionLocal()
+    try:
+        user = UserModel(email="agg@gmail.com", password_hash="123")
+        db.add(user)
+        db.flush()
+        venue = VenueModel(name="Main Venue1", address="Kristal1", city="Main City1")
+        db.add(venue)
+        db.flush()
+        seat1 = SeatModel(venue_id=venue.id, section="Main", row_label="A", seat_number=10)
+        db.add(seat1)
+        db.flush()
+        event1 = EventModel(venue_id=venue.id, name="New Event1", start_time=datetime(
+            2026, 9, 10, 20, 0,
+            tzinfo=ZoneInfo("Europe/Athens")), end_time=datetime(
+            2026, 9, 10, 22, 0,
+            tzinfo=ZoneInfo("Europe/Athens")
+        ))
+        db.add(event1)
+        db.flush()
+        event_seat1 = EventSeatModel(seat_id=seat1.id, event_id=event1.id, price=10.0, status=EventSeatStatus.HELD,hold_expires_at=datetime.now(ZoneInfo("Europe/Athens"))+timedelta(minutes=10))
+        db.add(event_seat1)
+        db.flush()
+        event_seat1_id=event_seat1.id
+        reservation = ReservationModel(event_seat_id=event_seat1.id, user_id=user.id,
+                                       expires_at=datetime.now(ZoneInfo("Europe/Athens")) - timedelta(minutes=1))
+        db.add(reservation)
+        db.flush()
+        reservation_id=reservation.id
+        db.commit()
+        barrier=Barrier(2)
+        thread1=Thread(target=cancel,args=(reservation.id,barrier))
+        thread2=Thread(target=cancel,args=(reservation.id,barrier))
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
+        assert results.count("success") == 1
+        assert results.count(409) == 1
+        check_db=SessionLocal()
+        try:
+            reservation = check_db.query(ReservationModel).filter(ReservationModel.id == reservation_id).first()
+            saved_event_seat1 = check_db.query(EventSeatModel).filter(EventSeatModel.id == event_seat1_id).first()
+            assert reservation.status == ReservationStatus.EXPIRED
+            assert saved_event_seat1.status == EventSeatStatus.AVAILABLE
+            assert saved_event_seat1.hold_expires_at is  None
+        finally:
+            check_db.close()
+    finally:
+        db.rollback()
+        db.close()
+
+def cancel(reservation_id,barrier):
+    check_db = SessionLocal()
+    try:
+        barrier.wait()
+        expire_reservation_service(reservation_id,check_db)
+        results.append("success")
+    except HTTPException as ex:
+        results.append(ex.status_code)
+    finally:
+        check_db.close()
