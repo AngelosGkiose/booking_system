a7a2883f4aebe2512b2cb0fd393c51237590cd0c
Aggelos Gkiose <it2023009@hua.gr>
2026-09-12 12:49:54 +0300

    Add JWT access token decoding

--- backend/app/security/jwt.py
+++ backend/app/security/jwt.py
@@ -2,6 +2,8 @@ from datetime import datetime, timezone, timedelta
 
 
 import jwt
+from fastapi import HTTPException
+from starlette import status
 
 from app.config import  settings
 
@@ -11,5 +13,18 @@ def create_access_token(data: dict):
     exp_time=datetime.now(timezone.utc)+ timedelta(minutes=settings.expiration_time)
     payload["exp"]=exp_time
     return jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
+
+
+def decode_access_token(token):
+    try:
+        payload=jwt.decode(token, settings.secret_key, algorithm=[settings.algorithm])
+        user_id=payload.get("sub")
+        if not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        return user_id
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
 
