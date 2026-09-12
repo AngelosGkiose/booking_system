acfea7a37e57499f72879b02d0b30ae6482e6735
Aggelos Gkiose <it2023009@hua.gr>
2026-09-12 20:44:15 +0300

    Add create reservation endpoint

--- backend/app/routers/reservations.py
+++ backend/app/routers/reservations.py
@@ -1,6 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from starlette import status
+
+from app.dependencies.get_current_user import get_current_user
+from app.dependencies.get_db import get_db
+from app.models import UserModel
+from app.schemas.reservation import ReservationCreate, ReservationResponse
+from app.services.reservation_service import create_reservation_service
 
 router = APIRouter(prefix="/reservations", tags=["reservations"])
 
-@router.post("/")
-def
+@router.post("/create",response_model=ReservationResponse,status_code=status.HTTP_201_CREATED)
+def create_reservation(reservation_data: ReservationCreate,current_user:UserModel = Depends(get_current_user),db: Session = Depends(get_db)):
+    return create_reservation_service(reservation_data.event_seat_id,db,current_user)
