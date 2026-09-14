dfdffd8f073e105d7eee5f8dd8424d1eacbfdd77
Aggelos Gkiose <it2023009@hua.gr>
2026-09-14 14:35:09 +0300

    Add confirm reservation endpoint

--- backend/app/routers/reservations.py
+++ backend/app/routers/reservations.py
@@ -6,10 +6,15 @@ from app.dependencies.get_current_user import get_current_user
 from app.dependencies.get_db import get_db
 from app.models import UserModel
 from app.schemas.reservation import ReservationCreate, ReservationResponse
-from app.services.reservation_service import create_reservation_service
+from app.services.reservation_service import create_reservation_service, confirm_reservation_service
 
 router = APIRouter(prefix="/reservations", tags=["reservations"])
 
 @router.post("/create",response_model=ReservationResponse,status_code=status.HTTP_201_CREATED)
 def create_reservation(reservation_data: ReservationCreate,current_user:UserModel = Depends(get_current_user),db: Session = Depends(get_db)):
     return create_reservation_service(reservation_data.event_seat_id,db,current_user)
+
+
+@router.post("/{reservation_id}/confirm",response_model=ReservationResponse,status_code=status.HTTP_200_OK)
+def confirm_reservation(reservation_id: int,current_user:UserModel = Depends(get_current_user),db: Session = Depends(get_db)):
+    return confirm_reservation_service(reservation_id,db,current_user)
