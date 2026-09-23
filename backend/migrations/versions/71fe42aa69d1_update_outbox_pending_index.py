42b25624e53528cf1500df122963fee4cbde8228
Aggelos Gkiose <it2023009@hua.gr>
2026-09-23 18:41:39 +0300

    Update outbox pending index

---
+++ backend/migrations/versions/71fe42aa69d1_update_outbox_pending_index.py
@@ -0,0 +1,39 @@
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "71fe42aa69d1"
+down_revision = "8034efd764fb"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+def upgrade():
+    op.execute(
+        "DROP INDEX IF EXISTS ix_outbox_events_unprocessed_created_at"
+    )
+
+    op.create_index(
+        "ix_outbox_events_pending_created_at",
+        "outbox_events",
+        ["created_at"],
+        unique=False,
+        postgresql_where=sa.text(
+            "processed_at IS NULL AND failed_at IS NULL"
+        ),
+    )
+def downgrade():
+    op.execute(
+        "DROP INDEX IF EXISTS ix_outbox_events_pending_created_at"
+    )
+
+    op.create_index(
+        "ix_outbox_events_unprocessed_created_at",
+        "outbox_events",
+        ["created_at"],
+        unique=False,
+        postgresql_where=sa.text(
+            "processed_at IS NULL"
+        ),
+    )
