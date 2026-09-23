662d3d01e98d7d9d0be479cd338d4591a1a81e1e
Aggelos Gkiose <it2023009@hua.gr>
2026-09-23 22:21:25 +0300

    Add health and readiness checks

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,28 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import text
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import Session
-from starlette import status
-
-from app.dependencies.get_db import get_db
+from fastapi import FastAPI
 from app.routers.reservations import router as reservations_router
 from app.routers.auth import router as auth
 from app.routers.monitoring import router as monitoring_router
-
+from app.routers.health_router import router as health_router
 app = FastAPI()
 
 app.include_router(reservations_router)
 app.include_router(auth)
 app.include_router(monitoring_router)
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
-
-@app.get("/health/database")
-def database_health_check(db: Session = Depends(get_db)):
-    try:
-        result = db.execute(text("SELECT 1")).scalar_one()
-        return {"status": "ok","data":result}
-    except SQLAlchemyError:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database not available")
+app.include_router(health_router)
---
+++ backend/app/routers/health_router.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter
+from fastapi import  Depends, HTTPException
+from redis import RedisError
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from starlette import status
+
+from app.dependencies.get_db import get_db
+from app.queue import redis_connection
+
+router = APIRouter(prefix="/health",tags=["health"])
+
+@router.get("/")
+def health_check():
+    return {"status": "ok"}
+
+
+@router.get("/redis")
+def redis_health_check():
+    try:
+        redis_connection.ping()
+        return {"status": "ok"}
+    except RedisError :
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,detail="Redis not available")
+
+@router.get("/database")
+def database_health_check(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1")).scalar_one()
+        return {"status": "ok"}
+    except SQLAlchemyError:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database not available")
+
+
+@router.get("/ready")
+def check_ready(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1")).scalar_one()
+        redis_connection.ping()
+        return {"status_database": "ok","status_redis":"ok"}
+    except SQLAlchemyError:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database not available")
+    except RedisError :
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,detail="Redis not available")
